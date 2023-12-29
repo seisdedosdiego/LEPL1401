58918cae722d5d4c2c2be05456ebbad16a443899
seisdedosdiego <111066813+seisdedosdiego@users.noreply.github.com>
2023-12-29 10:25:19 +0100

    Add files via upload

---
+++ session 6/Traitement d'exceptions.py
@@ -0,0 +1,12 @@
+try: 
+    parameters = command.split ()
+    if parameters[0] == "divide":
+        print ( "The value of your division is: " + str ( float(parameters[1])/float(parameters[2])))
+    elif parameters[0] == "showfile":
+        file = open ( parameters[1] )
+        print ( file.read () )
+        file.close ()
+    else:
+        print("Command not found")
+except:
+    print("There was an error")
