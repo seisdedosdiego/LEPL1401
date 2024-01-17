c641395a342c41ebbbb47a88f4fe01065f80d7ab
seisdedosdiego <111066813+seisdedosdiego@users.noreply.github.com>
2024-01-17 16:16:15 +0100

    Update Recherche binaire.py

--- session 5/Recherche binaire.py
+++ session 5/Recherche binaire.py
@@ -1,26 +1,17 @@
-def binary_search ( course, list_of_courses ):
-    """ Cette fonction retourne tous les étudiants d'un cours donné à l'aide d'une liste courses.
-        pré: course un cours, list_of_courses une liste avec les cours et leurs élèves.
-        post: retourne le nom des étudiants du cours, ou une liste vide si le cours n'existe pas ou s'il n'y a pas d'étudiant.
-    """
+def binary_search(course,list_of_courses):
     first = 0
     last = len(list_of_courses)-1
     found = False
+    res = []
 
     while first<=last and not found:
         middle = (first + last)//2
         if list_of_courses[middle][0] == course:
             found = True
-            index_course = middle
+            res = list_of_courses[middle][1]
         else:
-            if course < str(list_of_courses[middle][0]):
+            if course < list_of_courses[middle][0]:
                 last = middle-1
             else:
                 first = middle+1
-    
-    if found == True:
-        list_stu = list_of_courses[middle][1]
-    else:
-        list_stu = []
-    
-    return list_stu
+    return res
