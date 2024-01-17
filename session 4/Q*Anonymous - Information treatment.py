b9c592e0d60bb1ced6ab4669d55d0e44d3f0519b
seisdedosdiego <111066813+seisdedosdiego@users.noreply.github.com>
2024-01-17 15:55:01 +0100

    Update Q*Anonymous - Information treatment.py

--- session 4/Q*Anonymous - Information treatment.py
+++ session 4/Q*Anonymous - Information treatment.py
@@ -1,22 +1,14 @@
 def treatment(data):
-    """ Cette fonction complète la fonction précédente (extract) en ajoutant '*' et un nombre pour les répétitions.
-        pré: data une chaîne de caractères.
-        post: une chaîne de caractères modifiée.
-    """
-    lst = data.split(" ")
-    new_data = [[lst[0], 1]]
-    
-    for i in lst[1:]:
-        if i == new_data[-1][0]:
-            new_data[-1][1] +=1
-        elif i == "":
-            pass
+    res = ""
+    ls = data.split()
+    prev = ls[0]
+    count = 1
+    for i in range(1,len(ls)):
+        if ls[i] == prev:
+            count +=1
         else:
-            new_data.append([i,1])
-    
-    for j,k in enumerate(new_data):
-        new_data[j][1] = str(k[1])
-    x = []
-    for m in new_data:
-        x.append("*".join(m))
-    return " ".join(x)   
+            res += str(ls[i-1]) + "*" + str(count) + " "
+            prev = ls[i]
+            count = 1
+    res += str(ls[-1]) + "*" + str(count)
+    return res
